4c65fd22056db5e6d5391bfe60fda0c3670150e0
Alex Cheung <alexc.sfu@gmail.com>
2022-05-12 09:13:18 +0000

    modified fetcher.js, insert code to determine if file already exists.

--- fetcher.js
+++ fetcher.js
@@ -1,3 +1,14 @@
+/**Implement a node app called fetcher.js.
+
+It should take two command line arguments:
+
+1. a URL
+2. a local file path
+It should download the resource at the URL to the local path on your machine. Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
+
+> node fetcher.js http://www.example.edu/ ./index.html
+Downloaded and saved 3261 bytes to ./index.html */
+
 const request = require('request');
 const fs = require('fs');
 const rl = require('readline').createInterface({
@@ -21,23 +32,29 @@ const fetch = (url, writeFunc) => {
   })
 };
 
+
 const write = (localFilePath, body) => {
-  //write body to specific path
-    if(localFilePath) {
+  // first check if file already exists
+  fs.access(localFilePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      // if error, it means file doesn't exist, write file directly
+      fs.writeFile(localFilePath, body, () => {
+        console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}`);
+        process.exit();
+        });
+    } else {
+      // if no error, it means file already exists, we have to ask the user if he/she wants to overwrite
       rl.question('File already exists. Do you want to overwrite? Press "Y/N"  ',(answer) => {
         if (answer === 'Y' || answer === 'y') {
           fs.writeFile(localFilePath, body, () => {
             console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}`);
             rl.close();
-            })
+          })
         } else process.exit();
       })
-    } else {
-      fs.writeFile(localFilePath, body, () => {
-        console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}`);
-        })
     }
-}
+  })
+};
 
 
 fetch(url, write);
